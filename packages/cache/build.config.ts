5ece89c1579aa65721e4a9d297488b4b470632d4
iza <59828082+izadoesdev@users.noreply.github.com>
2026-08-29 17:45:16 +0300

    chore(cache): remove dead @databuddy/cache package

--- packages/cache/build.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineBuildConfig } from "unbuild";
-
-export default defineBuildConfig({
-	name: "@databuddy/cache",
-	entries: ["./src/drizzle.ts"],
-	externals: ["drizzle-orm"],
-	declaration: true,
-});
